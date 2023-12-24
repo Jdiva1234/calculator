959ef0cf3c015bc0d68c86bcb3200d7bb1009b9f
Justice <unankajustice@gmail.com>
2023-12-24 23:17:03 +0000

    a calculator update new calculator working on the constructor using an array to create multiple numbers

--- src/Main.java
+++ src/Main.java
@@ -8,44 +8,80 @@ import java.util.Scanner;
 
 
 public class Main {
- private int number;
+    private int[] numbers;
 
- public Main(int number) {
-     this.number = number;
- }
+    public Main(int[] numbers) {
+        this.numbers = numbers;
+    }
 
-    public static int addition(int firstNumber, int secondNumber){
-        int result = firstNumber + secondNumber;
-        return result;
+    public void setNumber(int[] numbers) {
+        this.numbers = numbers;
     }
-    public static int substraction(int firstNumber, int secondNumber){
-        int answer = firstNumber - secondNumber;
-        return answer;
+
+    public int[] getNumber() {
+        return numbers;
     }
-    public static int multipliaction(int firstNumber, int secondNumber){
-        int output = firstNumber * secondNumber;
-        return output;
+
+    public int addition(int secondNumber) {
+        int add = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            add += numbers[i];
+        }
+        return add;
     }
-    public static int division(int firstNumber, int secondNumber){
-        int answers = firstNumber / secondNumber;
-        return answers;
+
+    public int subtraction(int secondNumber) {
+        int sub = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sub -= numbers[i];
+        }
+        return sub;
     }
-    public static int modulo(int firstNumber, int secondNumber){
-        int results = firstNumber % secondNumber;
-        return results;
+
+    public int multipliaction(int secondNumber) {
+        int multiply = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            multiply *= numbers[i];
+        }
+        return multiply;
     }
+
+    public int division(int secondNumber) {
+        int divide = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            divide /= numbers[i];
+        }
+        return divide;
+    }
+
+    public int modulo(int secondNumber) {
+        int mode = 0;
+        for (int i = 0; i < numbers.length; i++){
+            mode %= numbers[i];
+    }
+    return mode;
+
+}
     public static void main(String[] args) {
         Scanner cal = new Scanner(System.in);
-        System.out.println("Hello! What is your first number?");
-        int firstNumber = cal.nextInt();
+        System.out.println("Hello! What are your numbers?");
+
+        int firstNumber = cal.nextInt();//why can't i use an array here?
+
+        /** THESE ARE THE TWO WAYS TO CREATE AN OBJECT
+        Main calculator = new Main(firstNumber);
+        Main calculator2 = new Main(firstNumber)
+         */
+        Main calculator = new Main(new int[]{firstNumber});//why is this an array?
+
 
         System.out.println("What is the second number");
         int secondNumber = cal.nextInt();
-        System.out.println("the two numbers added " + addition(firstNumber, secondNumber));
-        System.out.println("the two numbers subtracted " + substraction(firstNumber, secondNumber));
-        System.out.println("the two numbers multiplied " + multipliaction(firstNumber, secondNumber));
-        System.out.println("the two numbers divided " + division(firstNumber, secondNumber));
-        System.out.println("the two numbers modulo " + modulo(firstNumber, secondNumber));
+        System.out.println("the numbers added " + calculator.addition(secondNumber));
+        System.out.println("the numbers subtracted " + calculator.subtraction(secondNumber));
+        System.out.println("the numbers multiplied " + calculator.multipliaction(secondNumber));
+        System.out.println("the numbers divided " + calculator.division(secondNumber));
+        System.out.println("the numbers modulo " + calculator.modulo(secondNumber));
         cal.close();
 }
     }
