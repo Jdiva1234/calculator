a5f9420c21315afe90085dd24eab8c10098b7744
Justice <unankajustice@gmail.com>
2023-12-24 22:07:55 +0000

    a calculator update new calculator working on the constructor

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,23 @@
 //TODO: Use scanner to create an input interaction
 //TODO: Then use G
 import java.util.Scanner;
+
+
 public class Main {
+ private int number;
+
+ public Main(int number) {
+     this.number = number;
+ }
 
     public static int addition(int firstNumber, int secondNumber){
         int result = firstNumber + secondNumber;
         return result;
-
     }
     public static int substraction(int firstNumber, int secondNumber){
         int answer = firstNumber - secondNumber;
         return answer;
     }
-
     public static int multipliaction(int firstNumber, int secondNumber){
         int output = firstNumber * secondNumber;
         return output;
@@ -28,15 +33,15 @@ public class Main {
     public static int modulo(int firstNumber, int secondNumber){
         int results = firstNumber % secondNumber;
         return results;
-
     }
     public static void main(String[] args) {
         Scanner cal = new Scanner(System.in);
         System.out.println("Hello! What is your first number?");
         int firstNumber = cal.nextInt();
+
         System.out.println("What is the second number");
         int secondNumber = cal.nextInt();
-        System.out.println("the two numbers together " + addition(firstNumber, secondNumber));
+        System.out.println("the two numbers added " + addition(firstNumber, secondNumber));
         System.out.println("the two numbers subtracted " + substraction(firstNumber, secondNumber));
         System.out.println("the two numbers multiplied " + multipliaction(firstNumber, secondNumber));
         System.out.println("the two numbers divided " + division(firstNumber, secondNumber));
